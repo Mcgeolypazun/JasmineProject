daf9d30acf8ca70166973352fce43c81727899a9
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2023-10-10 20:01:30 +0900

    최종본

--- script/hanshin-header-script.js
+++ script/hanshin-header-script.js
@@ -10,6 +10,7 @@ $(function () {
         return false;
     });
 });
+
 $(document).ready(function () {
     let url = 'http://spartacodingclub.shop/sparta_api/weather/seoul'
     fetch(url).then(res => res.json()).then(data => {
